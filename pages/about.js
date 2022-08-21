824603c880453b102a34fce60c8fa00732690a55
Hampus Cousin <hampus_cousing@hotmail.com>
2022-08-21 08:48:11 +0200

    about page

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function about(props) {
     console.log(props)
   return (
-    <div>about</div>
+    <div>This is a test project and does not contain a buy option</div>
   )
 }
